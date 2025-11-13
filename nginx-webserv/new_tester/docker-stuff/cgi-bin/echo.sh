49bdf171271d2f80cc50dfafd7cf0906c9099e6a
mrbes25 <bastian123schmid@gmail.com>
2025-11-13 19:35:33 +0100

    trying new structure

---
+++ nginx-webserv/new_tester/docker-stuff/cgi-bin/echo.sh
@@ -0,0 +1,14 @@
+# cgi-bin/echo.sh
+#!/bin/sh
+echo "Status: 200 OK"
+echo "Content-Type: text/plain"
+echo ""
+echo "Method: $REQUEST_METHOD"
+echo "URI: $REQUEST_URI"
+echo "Query: $QUERY_STRING"
+echo "Content-Length: $CONTENT_LENGTH"
+echo "Content-Type: $CONTENT_TYPE"
+echo ""
+if [ "$REQUEST_METHOD" = "POST" ]; then
+  cat
+fi
