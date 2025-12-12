2a14b5234398207cfacee889985f113beeca88d5
Zarcross-dev <zarcross91@gmail.com>
2025-12-12 16:24:00 +0100

    changed exit function to a cgi_child_die function that kills the pid of the actual CGI subprocess running

--- src/classes/CGI.cpp
+++ src/classes/CGI.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <cstring>
 #include <sys/stat.h>
+#include <signal.h>
 
 CGIHandler::CGIHandler() : _env_array(NULL), _env_count(0), _env_capacity(0)
 {
@@ -106,7 +107,8 @@ void CGIHandler::_addEnvVar(const std::string& name, const std::string& value)
 	_env_capacity = _env_count;
 }
 
-void	CGIHandler::_setupEnvironment(const RequestHeader& request, const LocationConfig& location, const std::string& script_path)
+void	CGIHandler::_setupEnvironment(const RequestHeader& request,
+	const LocationConfig& location, const std::string& script_path)
 {
 	(void)location;
 	_cleanupEnvironment();
@@ -149,22 +151,37 @@ void	CGIHandler::_setupEnvironment(const RequestHeader& request, const LocationC
 	}
 }
 
+static void	cgi_child_die(int fd_in, int fd_out, char **env_for_exec)
+{
+	if (fd_in >= 0)
+		close(fd_in);
+	if (fd_out >= 0)
+		close(fd_out);
+	(void)env_for_exec;
+	kill(getpid(), SIGKILL);
+}
+
 std::string	CGIHandler::executeCGI(const std::string& script_path, const RequestHeader& request, const LocationConfig& location, const std::string& body)
 {
-	int	pipe_in[2], pipe_out[2];
+	int	pipe_in[2];
+	int	pipe_out[2];
+
 	if (pipe(pipe_in) == -1 || pipe(pipe_out) == -1)
 		return ("HTTP/1.1 500 Internal Server Error\r\n\r\nCGI pipe failed");
 	_setupEnvironment(request, location, script_path);
+
 	char	**env_for_exec = new char*[_env_count + 1];
 	for (int i = 0; i < _env_count; ++i)
 		env_for_exec[i] = _env_array[i];
 	env_for_exec[_env_count] = NULL;
-	
+
 	pid_t	pid = fork();
 	if (pid == -1)
 	{
-		close(pipe_in[0]); close(pipe_in[1]);
-		close(pipe_out[0]); close(pipe_out[1]);
+		close(pipe_in[0]);
+		close(pipe_in[1]);
+		close(pipe_out[0]);
+		close(pipe_out[1]);
 		delete[] env_for_exec;
 		return ("HTTP/1.1 500 Internal Server Error\r\n\r\nCGI fork failed");
 	}
@@ -174,14 +191,10 @@ std::string	CGIHandler::executeCGI(const std::string& script_path, const Request
 		close(pipe_in[1]);
 		close(pipe_out[0]);
 
-		if (dup2(pipe_in[0], STDIN_FILENO) == -1 ||
-			dup2(pipe_out[1], STDOUT_FILENO) == -1)
-		{
-			delete[] env_for_exec;
-			close(pipe_in[0]);
-			close(pipe_out[1]);
-			exit(1);
-		}
+		if (dup2(pipe_in[0], STDIN_FILENO) == -1
+			|| dup2(pipe_out[1], STDOUT_FILENO) == -1)
+			cgi_child_die(pipe_in[0], pipe_out[1], env_for_exec);
+
 		close(pipe_in[0]);
 		close(pipe_out[1]);
 
@@ -190,13 +203,15 @@ std::string	CGIHandler::executeCGI(const std::string& script_path, const Request
 		argv[1] = NULL;
 
 		execve(script_path.c_str(), argv, env_for_exec);
-		exit(1);
+
+		cgi_child_die(-1, -1, env_for_exec);
 	}
 	else
 	{
 		close(pipe_in[0]);
 		close(pipe_out[1]);
-		delete[] env_for_exec;		
+		delete[] env_for_exec;
+
 		if (!body.empty())
 			write(pipe_in[1], body.c_str(), body.length());
 		close(pipe_in[1]);
@@ -204,57 +219,48 @@ std::string	CGIHandler::executeCGI(const std::string& script_path, const Request
 		std::string	output;
 		char		buffer[1024];
 		ssize_t		bytes_read;
-		size_t		total_read = 0;
 
 		while ((bytes_read = read(pipe_out[0], buffer, sizeof(buffer) - 1)) > 0)
 		{
 			buffer[bytes_read] = '\0';
 			output += buffer;
-			total_read += bytes_read;
 		}
 		close(pipe_out[0]);
-		
+
 		int	status;
 		waitpid(pid, &status, 0);
+
 		if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
 		{
 			std::string	response;
 			size_t		header_end = output.find("\n\n");
-			
+
 			if (header_end == std::string::npos)
-			{
 				header_end = output.find("\r\n\r\n");
-			}
-			
+
 			if (header_end != std::string::npos)
 			{
 				std::string	cgi_headers = output.substr(0, header_end);
 				std::string	cgi_body;
-				
+
 				if (output.find("\r\n\r\n") != std::string::npos)
-				{
-					cgi_body = output.substr(header_end + 4); // Skip \r\n\r\n
-				}
+					cgi_body = output.substr(header_end + 4);
 				else
-				{
-					cgi_body = output.substr(header_end + 2); // Skip \n\n
-				}
-				
+					cgi_body = output.substr(header_end + 2);
+
 				response = "HTTP/1.1 200 OK\r\n";
 				response += cgi_headers;
-				// Replace any \n with \r\n for HTTP compliance
+
 				size_t pos = 0;
 				while ((pos = response.find("\n", pos)) != std::string::npos)
 				{
-					if (pos == 0 || response[pos-1] != '\r')
+					if (pos == 0 || response[pos - 1] != '\r')
 					{
 						response.insert(pos, "\r");
 						pos += 2;
 					}
 					else
-					{
 						pos++;
-					}
 				}
 				response += "\r\n\r\n" + cgi_body;
 			}
@@ -264,12 +270,9 @@ std::string	CGIHandler::executeCGI(const std::string& script_path, const Request
 				response += "Content-Type: text/html\r\n";
 				response += "\r\n" + output;
 			}
-			
 			return (response);
 		}
-		else
-		{
-			return ("HTTP/1.1 500 Internal Server Error\r\n\r\nCGI execution failed");
-		}
+		return ("HTTP/1.1 500 Internal Server Error\r\n\r\nCGI execution failed");
 	}
+	return ("HTTP/1.1 500 Internal Server Error\r\n\r\nCGI execution failed");
 }
